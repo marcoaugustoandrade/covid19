f553c5a21f4677ee89b6050bdd058afa1e33bf77
Marco Andrade <marco.andrade@ifro.edu.br>
2020-05-29 20:56:29 -0400

    Adicionando mapas

--- atualizar.sh
+++ atualizar.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 node /var/www/get-data.js
 date >> /var/www/atualizar.log
-
--- src/assets/js/functions.js
+++ src/assets/js/functions.js
@@ -179,4 +179,41 @@ fetch(url + '/dados')
         })
     })
 
-  
+// Mapas
+var mymap = L.map('mapid').setView([-12.734791, -60.132294], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFyY29hdWd1c3RvYW5kcmFkZSIsImEiOiJja2Fzd2RncnYwM3JqMnFuNHc0MGd5Ym1mIn0.YWOW66o-Wn_l_E6RnPJUgQ', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(mymap);
+
+// L.marker([51.5, -0.09]).addTo(mymap)
+//     .bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+
+L.circle([-12.734791, -60.132294], 500, {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5
+}).addTo(mymap).bindPopup('Casos confirmados: ');
+
+// L.polygon([
+//     [51.509, -0.08],
+//     [51.503, -0.06],
+//     [51.51, -0.047]
+// ]).addTo(mymap).bindPopup("I am a polygon.");
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mymap);
+}
+
+mymap.on('click', onMapClick);
